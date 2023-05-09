a696056346e2ac14d1883c98893a4499f2d63f01
Senna Smith <sennaasmith@gmail.com>
2023-05-08 22:58:32 -0400

    Create MyNameIs.java

---
+++ MyNameIs.java
@@ -0,0 +1,23 @@
+/**
+ * app
+ */
+public class MyNameIs {
+
+    public static void main(String[] args) {
+        System.out.println("My name is\n");
+        System.out.println("Senna\n");
+        System.out.println("Smith");
+    }
+}
+
+class Vacation { 
+    public static void main(String[] args) {
+        System.out.println("I'd like to visit\n");
+        System.out.println("Mexico\n");
+        System.out.println("Tampico\n");
+    }
+}
+
+class HiFriend {
+    
+} 
